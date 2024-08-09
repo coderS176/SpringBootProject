4fa742d0b074b790f64a32c4d25223d9fdbc7468
Shivam Gupta <74005091+coderS176@users.noreply.github.com>
2024-08-09 11:27:14 +0530

    Sample API Endpoints

---
+++ firstProject/src/main/java/com/example/firstProject/MyFirstClass.java
@@ -0,0 +1,17 @@
+package com.example.firstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyFirstClass {
+    @GetMapping("test")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("helloworld")
+    public String check() {
+        return "Hello!";
+    }
+}
